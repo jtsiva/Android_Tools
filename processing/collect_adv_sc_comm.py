c78c1738158698a2f95c3806b5e499a24702c5e1
Josh Siva <jtsiva@gmail.com>
2019-03-17 13:16:44 -0400

    updated processing script to handle old file name

--- processing/collect_adv_sc_comm.py
+++ processing/collect_adv_sc_comm.py
@@ -28,6 +28,25 @@ for filepath in glob.iglob(folder + '*scan_log*'):
     	# print (len(stats[index]))
     #
     index += 1
+if len(stats) == 0:
+	#some files were captured with this name
+	for filepath in glob.iglob(folder + '*scan_cap*'):
+		#print(filepath)
+		with open (filepath) as f:
+			first = True
+			for line in f:
+				if first:
+					first = False
+					stats.append([])
+				else:
+					stats[index].append(int(line.strip()))
+				#
+			#
+			# print ("number of records: "),
+			# print (len(stats[index]))
+		#
+		index += 1
+
 if len(stats) > 0:
 	folderName = os.path.basename(os.path.normpath(folder))
 	with open ("./output/" + folderName + "_comm_times.txt", "w") as f:
