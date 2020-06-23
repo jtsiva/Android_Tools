54e9992325db63b5ab17b1254d16fc7ca76e0040
Josh Siva <jtsiva@gmail.com>
2020-06-22 22:30:00 -0400

    updated run script to... actually work

--- run_multi.sh
+++ run_multi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-time python3 runTests.py -i ./tests/multi_client_$1.json -o ./quarantine/ -n $1 --sync
+time python3 runTests.py -i ./tests/multi_client_$1.json -o ./quarantine/ -n $(($1+1)) --sync
